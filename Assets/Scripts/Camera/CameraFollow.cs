7f967f760418e5465cf4549acf37c945a7063eac
Adan Moreno <morenoadan22@gmail.com>
2016-12-16 00:38:02 -0800

    Initial project commit

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//Calculate the initial offset
+		offset = transform.position - target.position;		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPosition = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPosition, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float speed = 6.0f;
+
+    Vector3 movement;
+    Animator playerAnimator;
+    Rigidbody playerRigidBody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake(){
+
+    	floorMask = LayerMask.GetMask("Floor");
+    	playerAnimator = GetComponent <Animator> ();
+    	playerRigidBody = GetComponent <Rigidbody> ();
+    }
+
+    void FixedUpdate(){
+    	float h = Input.GetAxisRaw("Horizontal");
+    	float v = Input.GetAxisRaw("Vertical");
+    	Move(h, v);
+    	Turning();
+    	Animating(h, v);
+    }
+
+    void Move(float h, float v){
+
+    	movement.Set(h, 0f, v);
+    	movement = movement.normalized * speed * Time.deltaTime;
+    	playerRigidBody.MovePosition ( transform.position + movement );    	    	
+    }
+
+    void Turning(){
+    	Ray camRay = Camera.main.ScreenPointToRay( Input.mousePosition );
+    	RaycastHit floorHit;
+    	if( Physics.Raycast ( camRay, out floorHit, camRayLength, floorMask) ){
+    		Vector3 playerToMouseRay = floorHit.point - transform.position;
+    		playerToMouseRay.y = 0f;
+
+    		Quaternion newRotation = Quaternion.LookRotation(playerToMouseRay);
+    		playerRigidBody.MoveRotation(newRotation);
+    	}
+    }
+
+    void Animating(float h, float v){
+    	bool isWalking = h != 0f || v != 0f;
+    	playerAnimator.SetBool("IsWalking", isWalking);
+    }
+
+}
