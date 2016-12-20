79555201a19d3c1d21957cc84b70d771551793d1
Adan Moreno <morenoadan22@gmail.com>
2016-12-19 23:12:15 -0800

    Add spawning locations

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -13,8 +13,8 @@ public class EnemyMovement : MonoBehaviour
     {
         // Set up the references.
         player = GameObject.FindGameObjectWithTag ("Player").transform;
-        // playerHealth = player.GetComponent <PlayerHealth> ();
-        // enemyHealth = GetComponent <EnemyHealth> ();
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <NavMeshAgent> ();
     }
 
@@ -22,16 +22,14 @@ public class EnemyMovement : MonoBehaviour
     void Update ()
     {
         // If the enemy and the player have health left...
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0){
             // ... set the destination of the nav mesh agent to the player.
             nav.SetDestination (player.position);
-        //}
+        }
         // Otherwise...
-        //else
-        //{
+        else{
             // ... disable the nav mesh agent.
-         //   nav.enabled = false;
-        //}
+           nav.enabled = false;
+        }
     } 
 }
